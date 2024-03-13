6c5fe4d7b853d2fcc17e83977307903b627ae930
Bryan Yang <bryan.yang255@gmail.com>
2024-03-13 14:07:34 +1100

    more work on evolutions display

--- src/PokemonEvolutionChain.tsx
+++ src/PokemonEvolutionChain.tsx
@@ -1,40 +1,54 @@
 import EvoCard from "./EvoCard";
+import prettifyName from "./prettifyName";
 import { Pokemon } from "./types";
 
 export default function PokemonEvolutionChain({ pokemon }: { pokemon: Pokemon }) {
 
+  function getRenderNodes() {
+    return pokemon.evolutionChain!.evolvesTo.map((entry) => {
+      if (entry.evolvesTo.length !== 0) {
+        return (
+          entry.evolvesTo.map((entry2) => {
+            return (
+              <div className="flex flex-row gap-10" key={pokemon.evolutionChain!.name}>
+                <EvoCard name={pokemon.evolutionChain!.name} />
+                <EvoCard name={entry.name} />
+                <EvoCard name={entry2.name} />
+              </div>
+            );
+          })
+        );
+      } else {
+        return (
+          <div className="flex flex-row gap-10" key={pokemon.evolutionChain!.name}>
+            <EvoCard name={pokemon.evolutionChain!.name} />
+            <EvoCard name={entry.name} />
+          </div>
+        );
+      }
+    })
+  }
+  if (pokemon.evolutionChain!.evolvesTo.length === 0) {
+    return (
+      <section
+        className="
+          p-4 text-center max-w-3xl flex flex-col gap-4
+          border border-t-0 border-slate-600 bg-slate-900
+        "
+      >
+        <span>{prettifyName(pokemon.name)} has no evolution chain.</span>
+      </section>
+    );
+  }
   return (
     <section
       className="
-        p-4 text-left max-w-3xl flex flex-col
+        p-4 text-left max-w-3xl flex flex-col gap-4
         border border-t-0 border-slate-600 bg-slate-900
       "
     >
-      <h2>Evolutions:</h2>
-      <div className="flex flex-row justify-evenly items-center gap-10">
-        <div className="max-w-[20%]">
-          <EvoCard name={pokemon.evolutionChain!.name} />
-        </div>
-        <div className="max-w-[20%]">
-          {pokemon.evolutionChain!.evolvesTo.map((entry) => {
-            return (
-              <EvoCard name={entry.name} key={entry.name} />
-            );
-          })}
-        </div>
-        <div className="max-w-[20%]">
-          {pokemon.evolutionChain!.evolvesTo.map((entry) => {
-            return (
-              <div key={entry.name}>
-                {entry.evolvesTo.length !== 0
-                  ? entry.evolvesTo.map((entry) => <EvoCard name={entry.name} key={entry.name} />) 
-                  : <></>
-                }
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <h2>Evolution Path</h2>
+      {getRenderNodes().map((entry) => entry)}
     </section>
   );
 }
